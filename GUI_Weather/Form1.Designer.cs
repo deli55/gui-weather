57d19ae0de854ebfdcb7c0def7f511ceee1b7733
Delinard <delinardrochabert@gmail.com>
2025-10-21 12:38:38 +0200

    Add project files.

---
+++ GUI_Weather/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GUI_Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFetchWeather = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lblApiKey = new System.Windows.Forms.Label();
+            this.txtApiKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFetchWeather
+            // 
+            this.btnFetchWeather.Location = new System.Drawing.Point(73, 179);
+            this.btnFetchWeather.Name = "btnFetchWeather";
+            this.btnFetchWeather.Size = new System.Drawing.Size(148, 57);
+            this.btnFetchWeather.TabIndex = 0;
+            this.btnFetchWeather.Text = "Check API";
+            this.btnFetchWeather.UseVisualStyleBackColor = true;
+            this.btnFetchWeather.Click += new System.EventHandler(this.btnFetchWeather_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.SystemColors.Control;
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtLog.Location = new System.Drawing.Point(464, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(336, 477);
+            this.txtLog.TabIndex = 1;
+            // 
+            // lblApiKey
+            // 
+            this.lblApiKey.AutoSize = true;
+            this.lblApiKey.Location = new System.Drawing.Point(70, 38);
+            this.lblApiKey.Name = "lblApiKey";
+            this.lblApiKey.Size = new System.Drawing.Size(88, 16);
+            this.lblApiKey.TabIndex = 3;
+            this.lblApiKey.Text = "Enter API Key";
+            this.lblApiKey.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtApiKey
+            // 
+            this.txtApiKey.Location = new System.Drawing.Point(73, 79);
+            this.txtApiKey.Multiline = true;
+            this.txtApiKey.Name = "txtApiKey";
+            this.txtApiKey.Size = new System.Drawing.Size(296, 75);
+            this.txtApiKey.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.txtApiKey);
+            this.Controls.Add(this.lblApiKey);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnFetchWeather);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFetchWeather;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lblApiKey;
+        private System.Windows.Forms.TextBox txtApiKey;
+    }
+}
+
---
+++ GUI_Weather/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Weather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnFetchWeather_Click(object sender, EventArgs e)
+        {
+            string apiKey = txtApiKey.Text.Trim();
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                Log("❌ Please enter your OpenWeatherMap API key first.");
+                return;
+            }
+
+            await FetchWeatherAsync(apiKey);
+        }
+
+        private async Task FetchWeatherAsync(string apiKey)
+        {
+            string city = "Belgrade"; // default city
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    Log($"Fetching weather for {city}...");
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        Log("Success!");
+                        Log(json);
+                    }
+                    else
+                    {
+                        Log($"API request failed: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"Error: {ex.Message}");
+            }
+        }
+
+        private void Log(string message)
+        {
+            txtLog.AppendText($"{DateTime.Now:HH:mm:ss} - {message}{Environment.NewLine}");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
